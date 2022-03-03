f963c49c6ad3185349af653c95cf59fd8466c188
Tibor Végh <73848390+photovegh@users.noreply.github.com>
2022-03-03 16:50:37 +0100

    Termék buttonok megjelentek a munkaterületen, terméknév és ID-t megkapták. 😊😊😊

--- public/js/pult.js
+++ public/js/pult.js
@@ -1,32 +1,67 @@
-for (i = 0; i < 10; i++) {
+getdata();
+/* for (i = 0; i < 10; i++) {
     document.getElementById(
         "termek"
-    ).innerHTML += `<button type='button' class='btn btn-danger mt-3 m-1' id=${i}>JavaScript</button>`;
-    /* document.getElementById("termek").innerHTML +=
-        "<button type='button' class='btn btn-danger mt-3 m-1'>JavaScript</button>"; */
+    ).innerHTML += `<button type='button' class='btn btn-danger mt-3 m-1' id=${i}>hmmmm</button>`;
+} */
+/* INFO: termék button-ok felrajzolása STAR INFO: */
+function renderPersons() {
+    //let personsHTML = "";
+    for (const person of state.keszlet) {
+        personsHTML += `<button type='button' class='btn btn-danger mt-3 m-1' id = ${person.id}>${person.nev} ${person.id}</button>`;
+    }
+    /* for (const person of state.keszlet) {
+        personsHTML += `
+        <tr>
+        <td>${person.nev}</td>
+        </tr>
+        `;
+    } */
+    document.getElementById("termek").innerHTML = personsHTML;
+    //document.getElementById("ucso2").innerHTML = personsHTML;
 }
+/* INFO: termék button-ok felrajzolása END INFO: */
 
-document.getElementById("ezaz").addEventListener("click", displayDate);
+/* INFO: termék adatok bekáráse START INFO: */
+async function getdata() {
+    const response = await fetch("/dataread");
+    //document.getElementById("ucso").innerHTML = "";
+    let persons;
+    state.keszlet = await response.json();
+    console.log(state.keszlet[0].nev);
+    renderPersons();
+
+    $(document).ready(function () {
+        $("button").click(function () {
+            console.log(this.id);
+            alert(this.id);
+            $(this).hide();
+        });
+    });
+}
+/* INFO: termék adatok bekáráse END INFO: */
+
+//document.getElementById("ezaz").addEventListener("click", displayDate);
 
 function displayDate() {
     document.getElementById("demo").innerHTML = Date();
 }
 
-$(document).ready(function () {
+/* INFO: $(document).ready(function () {
     $("button").click(function () {
         console.log(this.id);
-        //alert(this.id);
+        alert(this.id);
         $(this).hide();
     });
-});
+}); */
 
-$(document).ready(function () {
+/* $(document).ready(function () {
     $("p").click(function () {
         console.log(this.id);
         //alert(this.id);
         $(this).hide();
     });
-});
+}); */
 
 /* ####### FRONTEND SEND get REQUEST INFO: START INFO:*/
 console.log("dataread is ok ............");
@@ -35,28 +70,5 @@ const state = {
 };
 var personsHTML = "";
 /* ####### FRONTEND SEND get REQUEST  INFO: */
-async function getdata() {
-    const response = await fetch("/dataread");
-    document.getElementById("ucso").innerHTML = "";
-    let persons;
-    state.keszlet = await response.json();
-    console.log(state.keszlet[0].nev);
-    renderPersons();
-}
-function renderPersons() {
-    //let personsHTML = "";
-    for (const person of state.keszlet) {
-        personsHTML += `
-    <tr>
-    <td>${person.nev}</td>
-    <td>${person.beszar}</td>
-    
-    </tr>
-    `;
-    }
-    document.getElementById("ucso").innerHTML = personsHTML;
-    document.getElementById("ucso2").innerHTML = personsHTML;
-}
-getdata();
 
 /* ####### FRONTEND SEND get REQUEST INFO: END INFO:*/
